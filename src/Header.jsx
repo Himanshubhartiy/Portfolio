6b0cc7420ba37c4dce3322ede74f5cdb91ec6ff2
Himanshu Bhartiy <himanshubhartiy99@gmail.com>
2024-10-19 23:39:05 +0530

    add links and correct navbar

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,6 @@ function Header() {
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
-          {/* Use Link instead of anchor tags for internal navigation */}
           <Link to="/" className="navbar-brand">
             Portfolio
           </Link>
@@ -14,36 +13,36 @@ function Header() {
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item me-2 rounded-pill px-3">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
                 <Link to="/" className="nav-link">
                   Home
                 </Link>
               </li>
-              <li className="nav-item me-2 rounded-pill px-3">
+              <li className="nav-item">
                 <a className="nav-link" href="#about__section">
                   About Me
                 </a>
               </li>
-              <li className="nav-item me-2 rounded-pill px-3">
+              <li className="nav-item">
                 <a className="nav-link" href="#project__section">
                   Projects
                 </a>
               </li>
-              <li className="nav-item me-2 rounded-pill px-3">
+              <li className="nav-item">
                 <a className="nav-link" href="#skill__section">
                   Skills
                 </a>
               </li>
-              <li className="nav-item me-2 rounded-pill px-3">
+              <li className="nav-item">
                 <a className="nav-link" href="#contact__section">
                   Contact
                 </a>

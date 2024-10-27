0a24e28961f3de080f8e75810672667a3c5bc17c
Himanshu Bhartiy <himanshubhartiy99@gmail.com>
2024-10-27 22:05:15 +0530

    make it responsive

--- src/Video.jsx
+++ src/Video.jsx
@@ -6,7 +6,6 @@ function Video() {
     <div className="container mt-5">
       <div className="row justify-content-center">
         <div className="col-12 col-md-10 col-lg-8">
-          {/* Display video in full width of the column */}
           <video className="w-100 h-auto" controls>
             <source src="/video/kick.mp4" type="video/mp4" />
             Your browser does not support the video tag.
